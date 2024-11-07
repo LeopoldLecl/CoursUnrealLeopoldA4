9b14ed108a62a0be27762809b0d4b8a6fbaf0c63
leopoldlecl <leopold@leclerc.biz>
2024-11-07 12:01:23 +0100

    59

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/MyDeveloperSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "MyDeveloperSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHUE_API UMyDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+};
